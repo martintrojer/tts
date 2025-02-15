096a1e62507366164ed78fe73938716c261ab601
Martin Trojer <martin.trojer@gmail.com>
2025-02-15 11:15:32 +0000

    initial commit

---
+++ tts.py
@@ -0,0 +1,32 @@
+#!/Users/martintrojer/hacking/tts/venv/bin/python
+import subprocess
+import sys
+from tempfile import NamedTemporaryFile
+
+import numpy as np
+import soundfile as sf
+from kokoro import KPipeline
+
+
+def generate_audio(text: str, voice="af_heart", speed=1):
+    # 🇺🇸 'a' => American English, 🇬🇧 'b' => British English
+    pipeline = KPipeline(
+        lang_code="b", device="mps"
+    )  # <= make sure lang_code matches voice
+
+    generator = pipeline(text, voice=voice, speed=speed)
+    for gs, ps, audio in generator:
+        yield audio
+
+
+if __name__ == "__main__":
+    text = sys.stdin.read().strip()
+    all_audio = np.array([])
+    for audio in generate_audio(text):
+        all_audio = np.append(all_audio, audio)
+    with NamedTemporaryFile(suffix=".wav", delete=True) as tmpfile:
+        sf.write(tmpfile.name, all_audio, 24000)
+        print(f"Playing audio {tmpfile.name}")
+        subprocess.run(
+            ["vlc", "--play-and-exit", tmpfile.name],
+        )
